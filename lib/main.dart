4057599281afad3566a9430716ff51caf9172864
Kabir Jain <kabirjain15@gmail.com>
2021-11-09 01:54:43 +0530

    Design finalized

--- lib/main.dart
+++ lib/main.dart
@@ -5,10 +5,13 @@ import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'dart:convert';
+import 'package:google_fonts/google_fonts.dart';
 
-void main() => runApp(const MaterialApp(
+void main() => runApp(MaterialApp(
       title: "Weather App",
       home: Home(),
+      themeMode: ThemeMode.system,
+      darkTheme: ThemeData.dark(),
     ));
 
 class Home extends StatefulWidget {
@@ -24,9 +27,9 @@ class _HomeState extends State<Home> {
   var currently;
   var humidity;
   var windSpeed;
-
+  bool hasData = false;
   Future getWeather() async {
-    http.Response response = await http.get(Uri.parse(
+    var response = await http.get(Uri.parse(
         "https://api.openweathermap.org/data/2.5/weather?q=Pune&units=metric&appid=5a43765bd638623dca26642fffa81d6a"));
     var results = jsonDecode(response.body);
     setState(() {
@@ -35,6 +38,7 @@ class _HomeState extends State<Home> {
       currently = results['weather'][0]['main'];
       humidity = results['main']['humidity'];
       windSpeed = results['wind']['speed'];
+      hasData = true;
     });
   }
 
@@ -47,79 +51,161 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Container(
-            height: MediaQuery.of(context).size.height / 3,
-            width: MediaQuery.of(context).size.width,
-            color: Colors.red,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              // ignore: prefer_const_literals_to_create_immutables
-              children: [
-                Padding(
-                  padding: EdgeInsets.only(bottom: 10.0),
-                  child: Text(
-                    "Currently in India",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 14,
-                        fontWeight: FontWeight.w600),
-                  ),
-                ),
-                Text(
-                  "${temp.toString()}\u00B0",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 40,
-                      fontWeight: FontWeight.w600),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 10.0),
-                  child: Text(
-                    currently.toString(),
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 14,
-                        fontWeight: FontWeight.w600),
-                  ),
-                )
-              ],
-            ),
-          ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: ListView(
-                // ignore: prefer_const_literals_to_create_immutables
+        body: hasData == true
+            ? Column(
                 children: [
-                  ListTile(
-                    leading: FaIcon(FontAwesomeIcons.thermometerHalf),
-                    title: Text("Temperature"),
-                    trailing: Text("${temp.toString()}\u00B0"),
+                  Container(
+                    height: MediaQuery.of(context).size.height / 3,
+                    width: MediaQuery.of(context).size.width,
+                    color: Colors.red,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      // ignore: prefer_const_literals_to_create_immutables
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.only(bottom: 10.0),
+                          child: Text(
+                            "Currently in India",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 30,
+                                fontWeight: FontWeight.w600,
+                                fontFamily: GoogleFonts.poppins().fontFamily),
+                          ),
+                        ),
+                        Text(
+                          "${temp.toString()}\u00B0",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 45,
+                              fontWeight: FontWeight.w600),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(top: 10.0),
+                          child: Text(
+                            currently.toString(),
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                                fontWeight: FontWeight.w600,
+                                fontFamily: GoogleFonts.poppins().fontFamily),
+                          ),
+                        )
+                      ],
+                    ),
                   ),
-                  ListTile(
-                    leading: FaIcon(FontAwesomeIcons.cloud),
-                    title: Text("Weather"),
-                    trailing: Text(description.toString()),
-                  ),
-                  ListTile(
-                    leading: FaIcon(FontAwesomeIcons.sun),
-                    title: Text("Humidity"),
-                    trailing: Text(humidity.toString()),
-                  ),
-                  ListTile(
-                    leading: FaIcon(FontAwesomeIcons.wind),
-                    title: Text("Wind Speed"),
-                    trailing: Text(windSpeed.toString()),
-                  ),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: ListView(
+                        // ignore: prefer_const_literals_to_create_immutables
+                        children: [
+                          Card(
+                            elevation: 2.0,
+                            child: ListTile(
+                              leading: FaIcon(
+                                FontAwesomeIcons.thermometerHalf,
+                                color: Colors.red[300],
+                              ),
+                              title: Text(
+                                "Temperature",
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  color: Colors.red[300],
+                                ),
+                              ),
+                              trailing: Text(
+                                "${temp.toString()}\u00B0",
+                                style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                    fontFamily:
+                                        GoogleFonts.poppins().fontFamily),
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Card(
+                            elevation: 2.0,
+                            child: ListTile(
+                              leading: FaIcon(
+                                FontAwesomeIcons.cloud,
+                                color: Colors.blue[500],
+                              ),
+                              title: Text(
+                                "Weather",
+                                style: TextStyle(
+                                    fontSize: 20, color: Colors.blue[500]),
+                              ),
+                              trailing: Text(
+                                description.toString(),
+                                style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                    fontFamily:
+                                        GoogleFonts.poppins().fontFamily),
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Card(
+                            elevation: 2.0,
+                            child: ListTile(
+                              leading: FaIcon(
+                                FontAwesomeIcons.sun,
+                                color: Colors.orange[600],
+                              ),
+                              title: Text(
+                                "Humidity",
+                                style: TextStyle(
+                                    fontSize: 20, color: Colors.orange[600]),
+                              ),
+                              trailing: Text(
+                                humidity.toString(),
+                                style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                    fontFamily:
+                                        GoogleFonts.poppins().fontFamily),
+                              ),
+                            ),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Card(
+                            elevation: 2.0,
+                            child: ListTile(
+                              leading: FaIcon(
+                                FontAwesomeIcons.wind,
+                                color: Colors.blueGrey[400],
+                              ),
+                              title: Text(
+                                "Wind Speed",
+                                style: TextStyle(
+                                    fontSize: 20, color: Colors.blueGrey[400]),
+                              ),
+                              trailing: Text(
+                                windSpeed.toString(),
+                                style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                    fontFamily:
+                                        GoogleFonts.poppins().fontFamily),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
                 ],
-              ),
-            ),
-          )
-        ],
-      ),
-    );
+              )
+            : Center(child: CircularProgressIndicator()));
   }
 }
