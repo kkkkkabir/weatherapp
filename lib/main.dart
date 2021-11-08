db4d90a8dac872060a4c1d1210e6211fdd2dc4a5
Kabir Jain <kabirjain15@gmail.com>
2021-11-08 23:46:04 +0530

    UI made

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'dart:ui';
+import 'package:http/http.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'dart:convert';
 
 void main() => runApp(const MaterialApp(
       title: "Weather App",
@@ -22,6 +28,70 @@ class _HomeState extends State<Home> {
             height: MediaQuery.of(context).size.height / 3,
             width: MediaQuery.of(context).size.width,
             color: Colors.red,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              // ignore: prefer_const_literals_to_create_immutables
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(bottom: 10.0),
+                  child: Text(
+                    "Currently in India",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 14,
+                        fontWeight: FontWeight.w600),
+                  ),
+                ),
+                Text(
+                  "52\u00B0",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 40,
+                      fontWeight: FontWeight.w600),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 10.0),
+                  child: Text(
+                    "Rain",
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 14,
+                        fontWeight: FontWeight.w600),
+                  ),
+                )
+              ],
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: ListView(
+                // ignore: prefer_const_literals_to_create_immutables
+                children: [
+                  ListTile(
+                    leading: FaIcon(FontAwesomeIcons.thermometerHalf),
+                    title: Text("Temperature"),
+                    trailing: Text("52\u00B0"),
+                  ),
+                  ListTile(
+                    leading: FaIcon(FontAwesomeIcons.cloud),
+                    title: Text("Weather"),
+                    trailing: Text("Weather"),
+                  ),
+                  ListTile(
+                    leading: FaIcon(FontAwesomeIcons.sun),
+                    title: Text("Humidity"),
+                    trailing: Text("12"),
+                  ),
+                  ListTile(
+                    leading: FaIcon(FontAwesomeIcons.wind),
+                    title: Text("Wind Speed"),
+                    trailing: Text("12"),
+                  ),
+                ],
+              ),
+            ),
           )
         ],
       ),
