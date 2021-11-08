a96f3768b81c7b75aae3457deb238eb570696781
Kabir Jain <kabirjain15@gmail.com>
2021-11-08 23:09:30 +0530

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MaterialApp(
+      title: "Weather App",
+      home: Home(),
+    ));
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height / 3,
+            width: MediaQuery.of(context).size.width,
+            color: Colors.red,
+          )
+        ],
+      ),
+    );
+  }
+}
